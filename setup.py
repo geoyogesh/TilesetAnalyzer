6dc38a4394ef6dceaed600dbc522bf7bd3a74d66
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-03 04:47:57 -0500

    wip(build): resolve import issues

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='tileset_analyzer',
-      version='0.0.15',
+      version='0.0.16',
       url='https://github.com/geoyogesh/tileset_analyzer',
       license='MIT',
       author='Yogesh Dhanapal',
