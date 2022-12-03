d393e078e4609a847ee6377c55ebf6d900995041
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-03 06:43:36 -0500

    chore(docs): incremented version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='tileset_analyzer',
-      version='0.0.16',
+      version='0.0.17',
       url='https://github.com/geoyogesh/tileset_analyzer',
       license='MIT',
       author='Yogesh Dhanapal',
