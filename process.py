09591bef82b017f04f33f742416a53aab9eccee3
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-02 20:20:49 -0500

    feat(docs): factory pattern to define common tileset source structure

--- process.py
+++ process.py
@@ -1,10 +1,9 @@
 
 
 
+from data_source.tile_source_factory import TilesetSourceFactory
 from utils.json_utils import write_json_file
-from data_source.mbtiles.mbtiles_utils import count_tiles, count_tiles_by_z
-from data_source.mbtiles.sqllite_utils import create_connection
-from entities.tileset_analysis_result import TilesetAnalysisResult
+
 
 
 
@@ -12,12 +11,10 @@ def main():
     print('started')
 
 
-    mbtile = 'data/maptiler-osm-2017-07-03-v3.6.1-us_virginia.mbtiles'
-    conn = create_connection(mbtile)
+    src_path = 'data/maptiler-osm-2017-07-03-v3.6.1-us_virginia.mbtiles'
 
-    result = TilesetAnalysisResult()
-    result.set_count_tiles_total(count_tiles(conn))
-    result.set_count_tiles_by_z(count_tiles_by_z(conn))
+    data_source = TilesetSourceFactory.get_tileset_source(src_path)
+    result = data_source.analyze()
 
     write_json_file(result.get_json(), 'output/analysis_result.json')
     print('completed')
