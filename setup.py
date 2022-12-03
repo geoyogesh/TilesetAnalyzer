e560439029899208c173e2d4fe964793390f3fcf
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-03 04:10:59 -0500

    wip(deps): update dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='tileset_analyzer',
-      version='0.0.13',
+      version='0.0.14',
       url='https://github.com/geoyogesh/tileset_analyzer',
       license='MIT',
       author='Yogesh Dhanapal',
