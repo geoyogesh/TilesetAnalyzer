94e5d3be5a476c618c57184d03cf849e2fc6ee6c
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-03 03:06:35 -0500

    wip(deps): update dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='tileset_analyzer',
-      version='0.0.7',
+      version='0.0.8',
       url='https://github.com/geoyogesh/tileset_analyzer',
       license='GNU-GPL',
       author='Yogesh Dhanapal',
@@ -21,4 +21,5 @@ setup(name='tileset_analyzer',
       long_description_content_type='text/markdown',
       zip_safe=False,
       classifiers=CLASSIFIERS,
+      install_requires=['fastapi >= 0.88.0', 'uvicorn[standard] >= 0.20.0'],
  )
