1f0f2cc9214f1abd348e7931ad6614cc3b1232f5
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-03 23:12:19 -0500

    fix(docs): fixing path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = [
 
 setup (
     name='tileset_analyzer',
-    version='0.0.22',
+    version='0.0.23',
     url='https://github.com/geoyogesh/tileset_analyzer',
     license='MIT',
     author='Yogesh Dhanapal',
