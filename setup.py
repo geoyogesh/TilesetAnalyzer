bec221b86559f1259b6f0f9e5351f37042e90191
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-04 13:56:38 -0500

    fix(docs): fixing the build ignoring eslint warnings during CI

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = [
 
 setup (
     name='tileset_analyzer',
-    version='0.0.27',
+    version='0.0.28',
     url='https://github.com/geoyogesh/tileset_analyzer',
     license='MIT',
     author='Yogesh Dhanapal',
