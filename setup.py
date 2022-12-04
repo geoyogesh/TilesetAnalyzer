30e474cbeb34528314a280adc8dad3ee4893a485
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-04 03:34:24 -0500

    chore(deps): nodejs version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = [
 
 setup (
     name='tileset_analyzer',
-    version='0.0.24',
+    version='0.0.25',
     url='https://github.com/geoyogesh/tileset_analyzer',
     license='MIT',
     author='Yogesh Dhanapal',
