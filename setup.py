2b8bb2a7639276edd3412e97817e29510ca382a9
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-18 04:18:42 -0500

    feat(ui): size chart by layer

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = [
 
 setup(
     name='tileset_analyzer',
-    version='0.0.34',
+    version='0.0.35',
     url='https://github.com/geoyogesh/tileset_analyzer',
     license='MIT',
     author='Yogesh Dhanapal',
