9c33ad7ad684fdb35bcf2d1d9118ee97320bef8e
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-20 08:23:34 -0500

    chore(docs): react router page redirect in fast api

--- tileset_analyzer/api/main_api.py
+++ tileset_analyzer/api/main_api.py
@@ -4,6 +4,8 @@ import uvicorn
 import os
 from pathlib import Path
 from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from starlette.responses import RedirectResponse, FileResponse
 
 UI_PATH = f'{Path(os.path.dirname(__file__)).parent}/static/ui'
 
@@ -19,7 +21,20 @@ def start_api(temp_folder):
     )
 
     app.mount("/api", StaticFiles(directory=temp_folder), name="api")
-    app.mount("/", StaticFiles(directory=UI_PATH, html=True), name="ui")
+
+    @app.get("/")
+    async def index():
+        return FileResponse(f'{UI_PATH}/index.html')
+
+    app.mount("/static", StaticFiles(directory=f'{UI_PATH}/static'), name="ui")
+
+    @app.get("/{file_name}.{file_path}")
+    async def root_files(file_name: str, file_path: str):
+        return FileResponse(f'{UI_PATH}/{file_name}.{file_path}')
+
+    @app.get("/{path_name:path}")
+    async def index_react_path(request: Request, path_name: str):
+        return FileResponse(f'{UI_PATH}/index.html')
     
     uvicorn.run(app, host="0.0.0.0", port=8080)
     
