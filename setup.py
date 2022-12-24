ba928375b90ffdf2f0247799cd84e45356afba76
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-24 14:25:37 -0500

    feat(ui): mapcompoments using react pure components to handle libremap gl js state

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = [
 
 setup(
     name='tileset_analyzer',
-    version='0.0.44',
+    version='0.0.45',
     url='https://github.com/geoyogesh/tileset_analyzer',
     license='MIT',
     author='Yogesh Dhanapal',
