b6a6425285d153bd654d2fe1bef06ff19b7b18cd
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-24 14:25:27 -0500

    feat(ui): mapcompoments using react pure components to handle libremap gl js state

--- webapp/tileset-analyzer-app/src/Info/map/MapCanvas.tsx
+++ webapp/tileset-analyzer-app/src/Info/map/MapCanvas.tsx
@@ -1,51 +1,91 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './../../map.scss';
 import { Map as MapLibreGlMap } from 'maplibre-gl'
 
-export default function MapCanvas() {
-    const mapContainer = useRef(null);
-    const map = useRef<MapLibreGlMap | null>(null);
-    const [lng] = useState(-78);
-    const [lat] = useState(37);
-    const [zoom] = useState(4);
 
-    useEffect(() => {
-        if (map.current) return; //stops map from intializing more than once
-        map.current = new maplibregl.Map({
-            container: mapContainer.current!,
+interface Location {
+    lng: number;
+    lat: number;
+    zoom: number
+}
+
+interface IProps {
+}
+
+interface IState {
+    loc: Location;
+}
+
+
+export default class MapCanvas extends React.PureComponent<IProps, IState> {
+    mapContainer = React.createRef<HTMLDivElement>();
+    map: MapLibreGlMap | null = null
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            loc: {
+                lng: -91.0479, lat: 36.7892, zoom: 4
+            }
+        };
+    }
+
+    componentDidMount() {
+        const { loc } = this.state;
+
+        this.map = new maplibregl.Map({
+            container: this.mapContainer.current!,
             style: {
                 version: 8,
                 sources: {
-                  osm: {
-                    type: 'raster',
-                    tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-                    tileSize: 256,
-                    attribution: 'Map tiles by <a target="_top" rel="noopener" href="https://tile.openstreetmap.org/">OpenStreetMap tile servers</a>, under the <a target="_top" rel="noopener" href="https://operations.osmfoundation.org/policies/tiles/">tile usage policy</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>'
-                  }
+                    osm: {
+                        type: 'raster',
+                        tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+                        tileSize: 256,
+                        attribution: 'Map tiles by <a target="_top" rel="noopener" href="https://tile.openstreetmap.org/">OpenStreetMap tile servers</a>, under the <a target="_top" rel="noopener" href="https://operations.osmfoundation.org/policies/tiles/">tile usage policy</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>'
+                    }
                 },
                 layers: [{
-                  id: 'osm',
-                  type: 'raster',
-                  source: 'osm',
+                    id: 'osm',
+                    type: 'raster',
+                    source: 'osm',
                 }],
             },
-            center: [lng, lat],
-            zoom: zoom
+            center: [loc.lng, loc.lat],
+            zoom: loc.zoom
         });
 
-        map.current.addControl(new maplibregl.NavigationControl({
+        this.map.addControl(new maplibregl.NavigationControl({
             showCompass: true,
             showZoom: true,
             visualizePitch: true
         }), 'top-right');
 
-    });
 
+        this.map.on('move', () => {
+            if (!this.map) return;
+            this.setState({ ...this.state, ...{
+                loc: {
+                    lng: Number(this.map.getCenter().lng.toFixed(4)),
+                    lat: Number(this.map.getCenter().lat.toFixed(4)),
+                    zoom: Number(this.map.getZoom().toFixed(2))
+                }
+            }});
+    });
+}
+
+
+render () {
+    const { loc } = this.state;
     return (
         <div className="map-wrap">
-            <div ref={mapContainer} className="map" />
+            <div className="sidebar">
+                Longitude: {loc.lng} | Latitude: {loc.lat} | Zoom: {loc.zoom}
+            </div>
+            <div ref={this.mapContainer} className="map" />
         </div>
     );
+}
+
 }
