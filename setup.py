bf6afe460c1db7bb7bd36eca54a5ecba95e72729
Yogesh Dhanapal <geoyogesh@gmail.com>
2022-12-25 11:05:52 -0500

    feat(docs): popup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = [
 
 setup(
     name='tileset_analyzer',
-    version='0.0.46',
+    version='0.0.47',
     url='https://github.com/geoyogesh/tileset_analyzer',
     license='MIT',
     author='Yogesh Dhanapal',
