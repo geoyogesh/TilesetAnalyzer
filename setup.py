029c35c53c82d15e3460aa8796337e6ac4f1390d
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-02-21 23:47:17 -0500

    chore(release): bump version for release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ CLASSIFIERS = [
 
 setup(
     name='tileset_analyzer',
-    version='0.0.66',
+    version='0.0.67',
     url='https://github.com/geoyogesh/tileset_analyzer',
     license='MIT',
     author='Yogesh Dhanapal',
